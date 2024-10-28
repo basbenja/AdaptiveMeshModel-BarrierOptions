b2c10ef2b78a583100bb88d4ee243c09195f6bdd
Benjamín Bas <bbas@ascentio.com.ar>
2024-10-27 23:37:57 -0300

    started amm

--- src/amm.py
+++ src/amm.py
@@ -1,48 +1,122 @@
 import numpy as np
+np.set_printoptions(precision=2, suppress=True)
 
+from utils.barrier_option import Option
 from utils.formulas import trend, p_u, p_d, p_m
+from utils.trinomial_utils import discount
+from utils.trinomial_utils import (
+    asset_price_tree,
+    condensed_option_prices
+)
+
+from tabulate import tabulate
+
+def next_multiple_of_4(n):
+    return ((n + 3) // 4) * 4
+
 
 # The Adaptive Mesh Model for valuing Barrier Options
 def barrier_AMM(
-    option_type: str,
-    barrier_type: str,
+    option: Option,
     S0: float,
-    K: float,
+    T: float,
     r: float,
     sigma: float,
-    T: float,
-    N: int,
-    H: float
+    M: int = 1
 ):
     """
-    Adaptive Mesh Model for valuing Barrier Options.
+    Trinomial model for pricing European Barrier Options.
 
     Args:
-        option_type (str): call or pur
-        barrier_type (str): up-and-out, up-and-in, down-and-out, down-and-in
+        option (Option): option to be priced
         S0 (float): initial stock price
-        K (float): strike price
         r (float): risk-free rate
-        sigma (float): volatility
         T (float): time to maturity
-        N (int): number of time steps
-        H (float): barrier price
+        sigma (float): volatility
+        M (int): number of levels of fine mesh to be considered.
     """
-    k = T/N     # Time step = Maturity / Number of time steps
+    lamda = 3
+    h = (2**M) * (np.log(S0) - np.log(option.H))    # Price step
+    k = T / int(((lamda*sigma**2)/h**2)*T)     # # Coarsest mesh time step
+    N = int(T / k)
 
-    # We want B_10 to correspond to the initial asset price. So, the value of h
-    # (the up and down moves) must be set to:
-    h = 2 * (np.log(S0) - np.log(H))
+    # 1. First, the coarsest lattice is constructed starting at the initial log
+    #    asset price X = ln(H) + h
+    alpha = trend(r, sigma)
+    pu = p_u(h, k, sigma, alpha)
+    pm = p_m(h, k, sigma, alpha)
+    pd = p_d(h, k, sigma, alpha)
 
-    # The first step is to construct the coarse-mesh lattice A to compute option
-    # values at all of the A nodes
-    # We already have everything we need to compute the coarse lattice:
-    A = np.zeros((2*N+1, N+1))
-    for i in range(N+1):
-        pass
-    # Next, the coars mesh is used to compute option values at ln(H) and ln(H) + h,
-    # for time intervals of length k/4
+    log_S = asset_price_tree(np.log(H) + h, N, h)
+    A = condensed_option_prices(N, log_S, option.K, option.H, pu, pm, pd, r, k)
 
-    # Finally, we fill in the remaning fine-mesh nodes for the price ln(H) + h/2
-    # at time steps of k/4
-    pass
+    # 2. Iterate through the different levels of fine mesh
+    for level in range(M):
+        N = 4*N + 1
+        B = np.full((3, N*4 + 1), np.nan)
+
+    # 2. The coarse-mesh is used to compute option values at ln(H) and ln(H) + h,
+    #    for time intervals of length k/4
+    # La cantidad de filas siempre van a ser 3
+    # La cantidad de columnas va a depender de qué tan fina es la malla
+    # B = np.full((3, N*4 + 1), np.nan)
+    # Compute the option values where the fine mesh is connected to the coarse mesh
+    # lattice
+    # for col in range(N*4 + 1):      # Recorro todas las columnas
+    #     # Chequeo si coincide exactamente con la malla gruesa:
+    #     if col % 4 == 0:
+    #         B[0, col] = A[N  , col//4]
+    #         B[2, col] = A[N+1, col//4]
+    #     else:
+    #         closest_4 = next_multiple_of_4(col)
+    #         new_k = k/4 * (closest_4 - col)
+    #         pu = p_u(h, new_k, sigma, alpha)
+    #         pm = p_m(h, new_k, sigma, alpha)
+    #         pd = p_d(h, new_k, sigma, alpha)
+    #         B[0, col] = discount(
+    #             pu * A[N-1, closest_4//4] +
+    #             pm * A[N  , closest_4//4] +
+    #             pd * A[N+1, closest_4//4],
+    #             r, new_k
+    #         )
+    # B[2, :] = 0
+
+    # 3. We fill the remaining fine-mesh nodes for the price ln(H) + h/2 at time
+    #    steps of k/4.
+    # new_k = k / 4
+    # new_h = h / 2
+    # new_H = np.log(option.H) + new_h
+    # B[1, N*4] = 0
+    # pu = p_u(new_h, new_k, sigma, alpha)
+    # pm = p_m(new_h, new_k, sigma, alpha)
+    # pd = p_d(new_h, new_k, sigma, alpha)
+    # for col in range(N*4 - 1, -1, -1):
+    #     B[1, col] = discount(
+    #         pu * B[0, col+1] +
+    #         pm * B[1, col+1] +
+    #         pd * B[2, col+1],
+    #         r, new_k
+    #     )
+    # print(B[1,0])
+
+
+import json
+from utils.barrier_option import BarrierOption, BarrierType, PositionType
+with open("../config.json", "r") as f:
+    params = json.load(f)
+    option_type = params['option_type']
+    barrier_type = params['barrier_type']
+    position = params['position']
+    S0 = params['S0']
+    K = params['K']
+    T = params['T']
+    r = params['r']
+    sigma = params['sigma']
+    H = params['H']
+
+    option = BarrierOption(
+        type=option_type, K=K, T=T, position=PositionType.LONG,
+        barrier_type=BarrierType.DOWN_AND_OUT, H=H
+    )
+
+    barrier_AMM(option, S0, T, r, sigma)
